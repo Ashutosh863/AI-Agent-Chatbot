5fd679f8f3837a82c4b7c29b315c3f1b545d9b85
Ashutosh863 <ashutoshsankhala05@gmail.com>
2026-02-25 10:36:01 +0530

    Add files via upload

---
+++ frontend/app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import requests
+import uuid
+
+API_URL = "http://localhost:8000/chat"
+
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+st.title("AI Agent Chatbot")
+
+
+if st.button("New Chat"):
+    st.session_state.session_id = str(uuid.uuid4())
+    st.session_state.messages = []
+
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+
+user_input = st.chat_input("Type message")
+
+
+if user_input:
+
+    st.chat_message("user").write(user_input)
+
+    res = requests.post(
+        API_URL,
+        json={
+            "session_id": st.session_state.session_id,
+            "message": user_input
+        }
+    )
+
+    response = res.json()["response"]
+
+    st.chat_message("assistant").write(response)
+
+    st.session_state.messages.append(
+        {"role": "user", "content": user_input}
+    )
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response}
+    )
